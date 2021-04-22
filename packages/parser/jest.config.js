88ebeabe35b1f89a21ccdafe9f80b855c6e807c0
Rickard Natt och Dag <rickard@hey.com>
2021-04-22 16:25:26 +0200

    chore(ci): add ci build

--- packages/parser/jest.config.js
+++ packages/parser/jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   testEnvironment: 'node',
+  testPathIgnorePatterns: ['node_modules', 'out'],
   watchPlugins: [
     'jest-watch-typeahead/filename',
     'jest-watch-typeahead/testname',
